8c3837907ba550e44ba0b070254ebb89bd62223b
Devashish Lahariya <devashishlahariya9@gmail.com>
2022-03-05 16:18:12 +0530

    Update main.c

--- src/Src/main.c
+++ src/Src/main.c
@@ -1,15 +1,12 @@
-#include <string.h>
 #include "STM32F411xx_I2C.h"
+
+#define SLAVE_ADDR 0x68
 
 int main(void)
 {
-	uint8_t buffer[10];
-
-	memset(buffer, 0xAA, sizeof(buffer));
-
 	I2C_Init(I2C1, I2C1_USE_GPIOB8_9, I2C_FAST_MODE);
 
-	I2C_WriteByte(I2C1, 0x68, 0x02);
+	I2C_WriteByte(I2C1, SLAVE_ADDR, 0x02);
 
 	while(1)
 	{
